1d55ab8fea34a1bdb65699309f510dabe7234f9d
Hassan Nadeem <64688959+HassanNadeemWebD@users.noreply.github.com>
2023-05-29 13:08:05 +0500

    commit

--- FreshCart/dashboard/new.php
+++ FreshCart/dashboard/new.php
@@ -1,1 +1,19 @@
-<?php    print_r($_POST); ?>
+<pre>
+<?php   
+
+include 'config.php';
+$products = "SELECT `img`,`products`.`name` as `productName` ,`products`.`id` as `productID` , `catid`,
+`categories`.`name` as `categoryName` ,`createdAt`, `inStock`,
+ `products`.`status` as `productSatus` , `categories`.`status`as `categoryStatus`
+
+ FROM `products` INNER JOIN `categories` ON `products`.`catid` = `categories`.`id`";
+$res = mysqli_query($conn, $products);
+
+while($row = mysqli_fetch_assoc($res)){
+
+print_r($row);
+
+
+}
+
+?></pre>
