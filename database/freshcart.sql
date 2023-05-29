0ff0f4a777953c0d41e77f8914d403b461f6bd34
Hassan Nadeem <64688959+HassanNadeemWebD@users.noreply.github.com>
2023-05-29 12:20:40 +0500

    commit

--- database/freshcart.sql
+++ database/freshcart.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 24, 2023 at 10:08 AM
+-- Generation Time: May 29, 2023 at 09:19 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -58,6 +58,45 @@ CREATE TABLE `categories` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `icon`, `name`, `description`, `date`, `status`) VALUES
+(5, 'bakery.svg', 'bakery', 'bakery products', '2023-05-29', 1),
+(6, 'petfoods.svg', 'Pet Foods ', 'Pet Products', '2023-05-29', 1),
+(7, 'dairy.svg', 'Dairy', 'Dairy Products', '2023-05-30', 0),
+(8, 'snacks.svg', 'Snacks ', 'Snacks Products', '2023-05-31', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `catid` int(11) NOT NULL,
+  `status` int(11) NOT NULL,
+  `price` int(11) NOT NULL,
+  `createdAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `img` varchar(255) NOT NULL,
+  `inStock` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `description`, `catid`, `status`, `price`, `createdAt`, `img`, `inStock`) VALUES
+(1, '5 Star Chocolate', '5 Star Chocolate', 8, 1, 22, '2023-05-29 07:17:29', 'product-img-3.jpg', 1),
+(2, 'Butter', 'Butter', 7, 1, 22, '2023-05-29 07:17:29', 'product-img-10.jpg', 1),
+(3, 'Cat Feed', 'Pet Food', 6, 1, 50, '2023-05-29 07:09:59', 'product-img-9.jpg', 0),
+(4, 'Lays', 'Snacks', 8, 1, 50, '2023-05-29 07:17:29', 'product-img-4.jpg', 1),
+(5, 'Pop Corn', 'Snacks', 8, 1, 50, '2023-05-29 07:16:32', 'product-img-5.jpg', 1);
+
+--
 -- Indexes for dumped tables
 --
 
@@ -74,6 +113,13 @@ ALTER TABLE `categories`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `catid` (`catid`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -87,7 +133,23 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`catid`) REFERENCES `categories` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
